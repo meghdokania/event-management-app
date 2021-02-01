7f8492b819eae72f4518d467bdf76747ddd43506
Sayam Rakshit <sayamrakshit.eee18@itbhu.ac.in>
2021-02-01 23:33:16 +0530

    Corrected Internal Links of Header

--- prastuti/prastuti/views.py
+++ prastuti/prastuti/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from events import models as eventModel
 
+
 def Home(request, nouse=None):
     events = eventModel.Event.objects.all()
-    return render(request, 'prastuti\home.html', {'events':events})
+    return render(request, 'prastuti/home.html', {'events': events})
